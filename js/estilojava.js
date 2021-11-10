ebdd65d1a4ff5b402a1d3bc86d044ba71210fdfd
nicolasbearzi <89425130+nicolasbearzi@users.noreply.github.com>
2021-11-10 20:55:26 -0300

    Add files via upload

---
+++ js/estilojava.js
@@ -0,0 +1,9 @@
+function modooscuro() {
+  document.body.style.backgroundColor = "grey"; document.getElementById("myP").style.color = "white"; document.getElementById("grey").style.backgroundColor = "grey";
+}
+function autor() {alert ("Autor Nicolas Bearzi")}
+
+
+function functionName() {
+
+}
